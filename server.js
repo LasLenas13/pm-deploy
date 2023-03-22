fd2d30b90a63936e896af7f44357cd13cdea9c61
Christian Pearce <cotblvd@gmail.com>
2023-03-22 16:32:49 -0600

    public

--- server.js
+++ server.js
@@ -6,19 +6,19 @@ app.use(express.json())
 app.use(cors())
 
 
-// app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public`))
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/pmdeploy.html')
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/pmdeploy.html')
+// })
 
-app.get('/css', (req, res) => {
-    res.sendFile(__dirname + '/page.css')
-})
+// app.get('/css', (req, res) => {
+//     res.sendFile(__dirname + '/page.css')
+// })
 
-app.get('/js', (req, res) => {
-    res.sendFile(__dirname + '/server.js')
-})
+// app.get('/js', (req, res) => {
+//     res.sendFile(__dirname + '/server.js')
+// })
 
 app.listen(4000, () => {
     console.log('app is up on 4000')
